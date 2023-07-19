a321e2fc4199b44ff3e741be33559e993a92fc6d
Beatriz Recari <beatrizrecari@gmail.com>
2023-07-19 15:34:03 +0200

    added solutions to the preparations section exercises

---
+++ solutions.js
@@ -0,0 +1,77 @@
+// Solution to exercise #1 in Preparations section
+
+/* 
+   1. Use node to run the foo.js file using node:
+
+  OUTPUT: bar
+
+
+   2. Copy and paste the code from foo.js into the node REPL
+
+  OUTPUT: 
+    let foo = 'bar'
+    undefined
+
+    console.log(foo)
+    bar
+    undefined
+
+    foo
+    'bar
+
+  
+   3. Copy and paste the code from foo.js into the Chrome console REPL
+
+   OUTPUT:
+
+   bar
+   'bar'
+*/
+
+// Identify the Constructors for each of the following methods and classify each method as either a "Static" or an "Instance" method:
+
+/* 
+
+  1. substring = {constructor: String, methodType: Instance Method}
+
+  2. create = {constructor: Object, methodType: Static Method}
+
+  3. fromCharCode = {constructor: String, methodType: Static Method}
+
+  4. slice = {constructor: [Array, String], methodType: Instance Method}
+
+  5. toString = {constructor: [String, Object, Array, Number], methodType: Instance Method}
+
+  */
+
+// Which of the following names satisfy the style guidelines for variable names? For the purposes of this question, constants are not variables
+
+/* 
+
+    SOLUTION: [index, lazyDog, operand2]
+
+    */
+
+// Which of the following names satisfy the style guidelines for function names?
+
+/*
+
+    SOLUTION: [index, lazyDog, operand2, CatName]
+
+  */
+
+// Which of the following names satisfy the style guidelines for constants used to represent magic numbers?
+
+/*
+
+    SOLUTION: [BIG_NUMBER]
+
+  */
+
+// Which of the following names don't satisfy the style guidelines for naming variables, functions, or constants?
+
+/*
+
+    SOLUTION: [snake_case, quick_Fox, 1stCharacter]
+
+  */
