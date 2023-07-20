6a5e0f82b1fe8389f28e40918da537543cd410a3
Beatriz Recari <beatrizrecari@gmail.com>
2023-07-20 17:01:34 +0200

    added solutions to exercises from the Basics section in the JS book

--- solutions.js
+++ solutions.js
@@ -1,3 +1,5 @@
+// PREPARATIONS
+
 // Solution to exercise #1 in Preparations section
 
 /* 
@@ -28,7 +30,7 @@
    'bar'
 */
 
-// Identify the Constructors for each of the following methods and classify each method as either a "Static" or an "Instance" method:
+// 2. Identify the Constructors for each of the following methods and classify each method as either a "Static" or an "Instance" method:
 
 /* 
 
@@ -44,7 +46,7 @@
 
   */
 
-// Which of the following names satisfy the style guidelines for variable names? For the purposes of this question, constants are not variables
+// 3. Which of the following names satisfy the style guidelines for variable names? For the purposes of this question, constants are not variables
 
 /* 
 
@@ -52,7 +54,7 @@
 
     */
 
-// Which of the following names satisfy the style guidelines for function names?
+// 4. Which of the following names satisfy the style guidelines for function names?
 
 /*
 
@@ -60,7 +62,7 @@
 
   */
 
-// Which of the following names satisfy the style guidelines for constants used to represent magic numbers?
+// 5. Which of the following names satisfy the style guidelines for constants used to represent magic numbers?
 
 /*
 
@@ -68,10 +70,149 @@
 
   */
 
-// Which of the following names don't satisfy the style guidelines for naming variables, functions, or constants?
+// 6. Which of the following names don't satisfy the style guidelines for naming variables, functions, or constants?
 
 /*
 
     SOLUTION: [snake_case, quick_Fox, 1stCharacter]
+
+  */
+
+// THE BASICS
+
+// 1. Concatenate two or more strings, one with your first name and one with your last, to create a string with your full name as its value. For example, if your name is John Doe, think about how you can put 'John' and 'Doe' together to get 'John Doe'.
+/*
+
+    'Beatriz ' + 'Recari'
+  
+  */
+
+// 2. Using arithmetic operators, get the individual digits of a 4-digit number like 4936:
+
+/*
+
+    > let number = 4936
+    > let ones = number % 10
+    > ones
+    = 6
+
+    > number = (number - ones) / 10
+    = 493
+
+    > let tens = number % 10
+    > tens
+    = 3
+
+    > number = (number - tens) / 10
+    = 49
+
+    > let hundreds = number % 10
+    > hundreds
+    = 9
+
+    > let thousands = (number - hundreds) / 10
+    > thousands
+    = 4
+
+  */
+
+// 3. Identify the data type for each of the following values:
+
+/*
+
+    'true': String
+    false: Boolean
+    1.5: Number
+    2: Number
+    undefined: Undefined
+    {foo: 'bar'}: Object
+
+  */
+
+// 4. Explain why this code logs '510' instead of 15:
+// console.log('5' + 10)
+
+/*
+
+    Anytime there is a '+' sign with a string in either side, JavaScript implicitly coerces that non string type into a string and results in a concatenation.
+    Result: the number 10 gets coerced into a string '10' and later on concatenated to '5' resulting in the string '510'
+  
+  */
+
+// 5. Refactor the code from the previous exercise to use explicit coercion, so it logs 15 instead
+
+/*
+
+    console.log(Number('5') + 10)
+
+  */
+
+// 6. Use the template literal syntax along with the expression Number('5') + 10 to log the following sentence to the console:
+// The value of 5 + 10 is 15.
+
+/*
+
+    console.log(`The value of 5 + 10 is ${Number('5') + 10}.`)
+
+  */
+
+// 7. Will an error occur if you try to access an array element with an index that is greater than or equal to the length of the array? For example:
+// let foo = ['a', 'b', 'c']
+// console.log(foo.length)
+// console.log(foo[3])
+
+/*
+
+    No, since when you try to access an index value greater than the length of the array, it will return 'undefined'
+
+  */
+
+// 8. Create an array named names that contains a list of pet names. For instance:
+
+/*
+
+    let names = [asta, butterscotch, pudding, neptune, darwin]
+
+  */
+
+// 9. Create an object named pets that contains a list of pet names and the type of animal. For instance:
+
+/*
+
+    let pets = {
+      asta: 'dog',
+      butterscotch: 'cat',
+      pudding: 'cat',
+      neptune: 'fish',
+      darwin: 'lizard',
+    }
+
+  */
+
+// 10. What value does the following expression evaluate to?
+// 'foo' === 'Foo'
+
+/*
+
+    false (case matters when comparing strings)
+
+  */
+
+// 11. What value does the following expression evaluate to?
+// parseInt('3.1415')
+
+/*
+
+    It evaluates to 3. parseInt converts a string that begins with an digit into a numeric equivalent. If it encouters a non digit such as '.' like in the example, it stops converting.
+
+  */
+
+// 12. What value does the following expression evaluate to?
+// '12' < '9'
+
+/*
+
+    The expression evaluates as true since the operands are strings, not numbers. When you compare two strings, JavaScript performs a character-by-character comparison going from left to right, so on the first comparison, it determines that '1' < '9', so '12' must be less than '9'.'
+    Had it been numbers, it would have evaluated to false
 
   */
