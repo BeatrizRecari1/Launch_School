09355e695fac596fb2776cba261b232cbaf40543
Beatriz Recari <beatrizrecari@gmail.com>
2024-10-18 18:51:11 -0400

    added Array's exercises

--- age.js
+++ age.js
@@ -9,3 +9,14 @@ console.log(`In 10 years, you will be ${age + 10} years old.`);
 console.log(`In 20 years, you will be ${age + 20} years old.`);
 console.log(`In 30 years, you will be ${age + 30} years old`);
 console.log(`In 40 years, you will be ${age + 40} years old.`);
+
+// Solution to exercise 1 of Loops and Iterating chapter
+
+let readlineSync = require('readline-sync');
+let age = readlineSync.question('How old are you? ');
+age = parseInt(age);
+console.log(`You are ${age} years old.`);
+
+for (let i = 0; index <= 40; index += 10) {
+  console.log(`In ${i} years, you will be ${age + i} years old.`)
+}
--- solutions.js
+++ solutions.js
@@ -531,4 +531,217 @@ show();
 
   */
 
+
+/* LOOPS AND ITERATIONS
+
+  1. Refer to age.js file for exercise number 1
+
+  2. Write a function that computes and returns the factorial of a number by using a for loop. The factorial of a positive integer n, signified by n!, is defined as the product of all integers between 1 and n, inclusive:
+
   
+
+    function factorialNumber (number) {
+      let result = 1
+      for (let counter = number; counter > 0; counter -= 1) {
+        let result *= counter
+        }
+        return result
+      }
+
+  3. The following code causes an infinite loop (a loop that never stops iterating). Why?
+  
+
+    let counter = 0;
+
+    while (counter = 1) {
+    console.log(counter);
+    counter += 1;
+
+      if (counter > 2) {
+      break;
+    }
+  }
+    ==> The problem is on like 556 where counter is assigned to 1 in the conditional part of the while loop. Counter = 1 will always evaluate to a truthy value and line 560 will not cause the program to stop since it says it must be bigger than 2 and line 558 will always make counter = 2. Line 559 'break' never runs, then goes back to line 556 where counter gets reassigned to 1 and infinitely continues in this pattern.
+
+  
+    4. Does the following code produce an error? Why or why not? What output does this code send to the console?
+
+      for (let i = 0; i < 5;) {
+    console.log(i += 1);
+  }
+
+  ==> The code doesn't produce an error since all 3 components of the for loop are optional. In this code, we omit the "next value" component; however, this isn't a problem here since we increment the loop variable on line 2. since we have the condition of i < 5, it will only print 1, 2, 3, 4, 5.
+
+  
+
+  5. The following code uses a randomNumberBetween function to generate a number between its first and second arguments. It uses a while loop to try to generate a number greater than 2. Refactor the code so that you don't need to call randomNumberBetween from two different locations, lines 6 and 10. Do not change the arguments you pass to randomNumberBetween.
+
+    function randomNumberBetween(min, max) {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    let tries = 0;
+    let result;
+
+    do {
+      result = randomNumberBetween(1, 6);
+      tries += 1;
+    } while (result <= 2);
+
+    console.log('It took ' + String(tries) + ' tries to get a number greater 2');
+
+  
+
+  6. Reimplement the factorial function from exercise 2 using recursion. Once again, you may assume that the argument is always a positive integer.
+
+    function factorial(number) {
+      if (number === 1) {
+        return 1;
+      }
+
+      return number * factorial(number - 1);
+    }
+
+
+/* ARRAYS
+
+  1. In the following code, what are the final length values for array1, array2, array3, array4, and array5?
+
+    let array1 = [1, 2, undefined, 4];  ==> 4
+
+    let array2 = [1];                   ==> 5
+    array2.length = 5;
+
+    let array3 = [];                    ==> 0
+    array3[-1] = [1];
+
+    let array4 = [1, 2, 3, 4, 5];       ==> 3
+    array4.length = 3;
+
+    let array5 = [];                    ==> 101
+    array5[100] = 3;
+
+
+
+  2. Log all of the even values from myArray to the console.
+
+    let myArray = [1, 3, 6, 11, 4, 2,
+                  4, 9, 17, 16, 0];
+    
+    ==> myArray.forEach(function(value) {
+          if (value % 2 === 0) {
+          console.log(value);
+        }
+      });
+
+
+  3. Let's make the problem a little harder. In this problem, we're again interested in even numbers, but this time the numbers are in nested arrays in a single outer array.
+
+    for (let i = 0; i < myArray.length; i += 1) {
+      for (let j = 0; j < myArray[i].length; j += 1) {
+        let value = myArray[i][j];
+        if (value % 2 === 0) {
+          console.log(value);
+        }
+      }
+    }
+
+    myArray.forEach(function(nestedArray) {
+      nestedArray.forEach(function(value) {
+        if (value % 2 === 0) {
+          console.log(value);
+        }
+      });
+    });
+
+  
+
+  4. Let's try another variation on the even-numbers theme. We'll return to the simpler one-dimensional array. In this problem, we want to use the map function to create a new array that contains one element for each element in the original array. If the element is an even value, then the corresponding element in the new array should contain the string 'even'; otherwise, the element in the new array should contain 'odd'.
+
+    myArray.map(function(value) {
+      if(value % 2 === 0) {
+        return 'even'}
+      else {
+        return 'odd'}
+    })
+        
+
+  5. Write a findIntegers function that takes an array argument and returns an array that contains only the integers from the input array. Use the filter method in your function.
+
+    let things = [1, 'a', '1', 3, NaN, 3.1415, -4, null, false];
+    let integers = findIntegers(things);
+    console.log(integers); // => [1, 3, -4]
+
+    ==> function findIntegers(array) {
+          return array.filter(function(element) {
+            return Number.isInteger(element)
+          })
+        }
+
+  
+  6. Use map and filter to first determine the lengths of all the elements in an array of string values, then discard the even values (keep the odd values).
+
+    let arr = ['a', 'abcd', 'abcde', 'abc', 'ab'];
+    console.log(oddLengths(arr)); // => [1, 5, 3]
+
+    ==> function oddLengths(strings) {
+          let lengths = strings.map((letters) => letters.length);
+          let oddLengths = lengths.filter((number) => number % 2 === 1);
+          return oddLengths;
+        }
+
+        let arr = ['a', 'abcd', 'abcde', 'abc', 'ab'];
+        console.log(oddLengths(arr));
+ 
+    
+  7.  Use reduce to compute the sum of the squares of all of the numbers in an array:
+
+    let array = [3, 5, 7];
+    console.log(sumOfSquares(array)); // => 83
+
+    ==> function sumOfSquares(numbers) {
+          return numbers.reduce((accumulator, number) => {
+            return accumulator + number * number;
+          }, 0);
+        }
+
+  
+  8. Write a function similar to the oddLengths function from Exercise 6, but don't use map or filter. Instead, try to use the reduce method.
+
+    function oddLengths(strings) {
+      return strings.reduce((filteredNumbersArray, letters) => {
+        let length = letters.length;
+        if (length % 2 === 1) {
+          filteredNumbersArray.push(length);
+        }
+
+        return filteredNumbersArray;
+      }, []);
+    }
+
+    let arr = ['a', 'abcd', 'abcde', 'abc', 'ab'];
+    console.log(oddLengths(arr));
+
+
+  9. Without using a for, while, or do/while loop, write some code that checks whether the number 3 appears inside these arrays:
+
+    let numbers1 = [1, 3, 5, 7, 9, 11];
+    let numbers2 = [];
+    let numbers3 = [2, 4, 6, 8];
+
+    ==>> numbers1.includes(3); // true
+         numbers2.includes(3); // false
+         numbers3.includes(3); // false
+    
+  
+  10. Write some code to replace the value 6 in the following array with 606
+
+      let arr = [
+        ["hello", "world"],
+        ["example", "mem", null, 6, 88],
+        [4, 8, 12]
+      ];
+
+    ==> arr[1][3] = 606
+
+    
