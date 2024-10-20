3429269287fae6372a5a9823686e598a88cd25eb
Beatriz Recari <beatrizrecari@gmail.com>
2024-10-19 20:18:56 -0400

    added Objects exercises

--- solutions.js
+++ solutions.js
@@ -745,3 +745,261 @@ show();
     ==> arr[1][3] = 606
 
     
+
+/* OBJECTS
+
+  1. Given the following code, how can you access the name of the person?
+
+      let person = {
+         name:       'Bob',
+        occupation: 'web developer',
+         hobbies:    'painting',
+      };
+
+      ==> person.name // person['name']
+
+  
+
+  2. Which of the following values are valid keys for an object?
+
+    1
+    '1'
+    undefined
+    'hello world'
+    true
+    'true'
+
+    ==> All of them, however, JS will coerce all the non string values to strings, making 1 and '1' as well as true and 'true' represent the same key.
+
+
+
+  3. Use object literal syntax (e.g., { key: value, ... } notation) to create an object that behaves as an array in a for statement. The object should contain at least 3 elements. You should place your code between the braces in the let statement:
+
+    let myArray = {
+    };
+
+    for (let i = 0; i < myArray.length; i += 1) {
+      console.log(myArray[i]);
+    }
+
+    ==> let myArray = {
+          0: 'a',
+          1: 'b',
+          2: 'c',
+          length: 3,
+        };
+
+        for (let i = 0; i < myArray.length; i += 1) {
+        console.log(myArray[i]);
+        }
+
+
+
+  4. Create an array from the keys of the object obj below, with all of the keys converted to uppercase. Your implementation must not mutate obj.
+
+    let obj = {
+      b: 2,
+      a: 1,
+      c: 3,
+    };
+
+    ==> let objKeys = Object.keys(obj);
+        let upperKeys = objKeys.map((key) => key.toUpperCase());
+        console.log(upperKeys); // => [ 'B', 'A', 'C' ]
+        console.log(obj); // => { b: 2, a: 1, c: 3 }
+
+
+  5. Create a new object named myObj that uses myProtoObj as its prototype.
+
+    let myProtoObj = {
+      foo: 1,
+      bar: 2,
+    };
+
+    ==> let myObj = Object.create(myProtoObj)
+
+
+
+  6. Which of the following values are primitive values? Which are objects? Which are neither?
+
+    "foo"                                 // primitive
+    3.1415                                // primitive
+    [ 'a', 'b', 'c' ]                     // object
+    false                                 // primitive
+    foo                                   // identifier ==> neither
+    function bar() { return "bar"; }      // object (functions are objects)
+    undefined                             // primitive  
+    { a: 1, b: 2 }                        // object
+
+
+
+  7. Add a qux property with value 3 to the myObj object we created in the previous exercise. Now, examine the following code snippets:
+
+    myObj.qux = 3
+
+
+    Are these to codes snippets logging the same?
+
+    let objKeys = Object.keys(myObj);
+    objKeys.forEach(function(key) {
+      console.log(key);
+    });
+
+    for (let key in myObj) {
+      console.log(key);
+    }
+
+    ==> No, the second code snippet logs in the myObj keys (qux) as well as the prototype object's keys.
+
+
+
+  8. Create a function that creates and returns a copy of an object. The function should take two arguments: the object to copy and an array of the keys that you want to copy. Do not mutate the original object.
+
+    The function should let you omit the array of keys argument when calling the function. If you do omit the argument, the function should copy all of the existing keys from the object.
+
+
+    function copyObj(sourceObject, keys) {
+      let destinationObject = {};
+
+      if (keys) {
+        keys.forEach(function(key) {
+        destinationObject[key] = sourceObject[key];
+      });
+
+      return destinationObject;
+        } else {
+        return Object.assign(destinationObject, sourceObject);
+      }
+    }
+
+
+
+  9. What does this code log to the console.
+
+    let foo = {
+      a: 'hello',
+      b: 'world',
+    };
+
+    let qux = 'hello';
+
+    function bar(argument1, argument2) {
+      argument1.a = 'hi';
+      argument2 = 'hi';
+    }
+
+    bar(foo, qux);
+
+    console.log(foo.a);
+    console.log(qux);
+
+    ==> 'hi' 'hello'
+
+
+
+  10. How many primitive values are there in the following expression? Identify them. How many objects are there in the expression? Identify those objects.
+
+    [1, 2, ["a", ["b", false]], null, {}]
+
+    ==> There are 6 primitive values and 4 objects
+    ==> The outermost set of brackets defines an array (an object) that contains 5 elements. The elements with values 1, 2, and null are all primitive values, while ["a", ["b", false]] is a nested array, and {} is nested object. The nested array has 2 elements, one of which is a primitive value ("a"), while the other is yet another nested array. Finally, this innermost array contains two elements, "b" and false, both of which are primitive values.
+
+
+
+  11. Write some code to replace the value 6 in the following object with 606:
+
+    let obj = {
+      foo: { a: "hello", b: "world" },
+      bar: ["example", "mem", null, { xyz: 6 }, 88],
+      qux: [4, 8, 12]
+    };
+
+    ==> obj.bar[3].xyz = 606
+
+
+
+  12. Consider the following code: As written, this code will raise an error on line 6. Without creating a new function or changing lines 5 or 6, update this code to work as intended.
+
+    function foo(bar) {
+      console.log(bar, bar, bar);
+    }
+
+    foo("hello"); // should print "hello hello hello"
+    bar("hi");    // should print "hi hi hi"
+
+  ==> function foo(bar) {
+        console.log(bar, bar, bar);
+      }
+
+      let bar = foo;
+
+      foo("hello"); // prints "hello hello hello"
+      bar("hi");    // prints "hi hi hi
+
+
+
+  13. Consider the following code: As written, this code will raise an error on line 5. Without modifying foo, update this code to print the desired text.
+
+    function foo(bar) {
+      console.log(bar());
+    }
+
+    foo();    // Should print 'Welcome'
+    foo();    // Should print 3.1415
+    foo();    // Should print [1, 2, 3]
+  
+  
+  ==> function foo(bar) {
+          console.log(bar());
+        }
+
+        foo(function() { return "Welcome" });
+        foo(function() { return 3.1415 });
+        foo(function() { return [1, 2, 3] });
+
+
+
+  14. Identify all of the variables, object property names, primitive values, and objects shown in the following code (assume the code has not been executed). Don't panic if you miss a few items - this exercise is more challenging than it looks.
+
+  function hello(greeting, name) {
+    return greeting + ' ' + name;
+  }
+
+  function xyzzy() {
+    return { a: 1, b: 2, c: [3, 4, 5], d: {} };
+  }
+
+  const howdy = hello('Hi', 'Grace');
+  let foo = xyzzy();
+
+  ==> Variables: hello, xyzzy, greeting, name, howdy, and foo. Remember that function names and parameters are variables. In addition, constants created with const are variables that can't be reassigned. Property names in an object are not variables.
+      Property Names: a, b, c, d. It's also worth noting that array indexes are property names, so 0, 1, and 2 are property names for the [3, 4, 5] array
+      Objects: The functions hello and xyzzy are both objects.
+
+
+
+  15. Identify all of the variables, object property names, primitive values, and objects in the following code. This problem is even more challenging than the previous one.
+
+    function bar(arg1, arg2) {
+      let foo = 'Hello';
+      const qux = {
+        abc: [1, 2, 3, [4, 5, 6]],
+        def: null,
+        ghi: NaN,
+        jkl: foo,
+        mno: arg1,
+        pqr: arg2,
+      };
+
+      return qux;
+    }
+
+let result = bar('Victor', 'Antonina');
+
+==> Variables: bar, arg1, arg2, foo, qux, and result.
+    Property Names: abc, def, ghi, jkl, mno, pqr, 0, 1, 2, and 3
+    Primitive values: 'Hello', 1, 2, 3, 4, 5, 6, null, NaN, 'Victor', and 'Antonina' are the most apparent primitive values. Since property names are strings in most cases, and strings are primitive values, we should also include 'abc', 'def', 'ghi', 'jkl'. 'mno', 'pqr', '0', '1', '2', and '3'.
+    Objects: The bar function, the array [1, 2, 3, [4, 5, 6]], the array [4, 5, 6], and the object assigned to qux on line 3 are all objects
+
+
+    
