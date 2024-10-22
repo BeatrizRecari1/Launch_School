e5d032a95c66d84a9afa635bb542a53c02b012d9
Beatriz Recari <beatrizrecari@gmail.com>
2024-10-22 10:17:10 -0400

    added the last more stuff exercises

--- solutions.js
+++ solutions.js
@@ -1002,4 +1002,121 @@ let result = bar('Victor', 'Antonina');
     Objects: The bar function, the array [1, 2, 3, [4, 5, 6]], the array [4, 5, 6], and the object assigned to qux on line 3 are all objects
 
 
-    
+
+
+
+/* MORE STUFF
+
+  1. What does this code log to the console? Why?
+
+    let array1 = [1, 2, 3];
+    let array2 = array1;
+    array1[1] = 4;
+    console.log(array2);
+
+    ==> It logs [1, 4, 3] 
+    ==> array1 and array2 reference the same array: if we change an element using array1, it also changes that element in array2. 
+
+
+
+  2. What do the following error message and stack trace tell you?
+
+    $ node exercise2.js
+    /Users/wolfy/tmp/exercise2.js:4
+    console.log(greeting);
+              ^
+
+    ReferenceError: greeting is not defined
+    at hello (/Users/wolfy/tmp/exercise2.js:4:15)
+    at Object.<anonymous> (/Users/wolfy/tmp/exercise2.js:13:1)
+    at Module._compile (internal/modules/cjs/loader.js:721:30)
+    at Object.Module._extensions..js (internal/modules/cjs/loader.js:732:10)
+    at Module.load (internal/modules/cjs/loader.js:620:32)
+    at tryModuleLoad (internal/modules/cjs/loader.js:560:12)
+    at Function.Module._load (internal/modules/cjs/loader.js:552:3)
+    at Function.Module.runMain (internal/modules/cjs/loader.js:774:12)
+    at executeUserCode (internal/bootstrap/node.js:342:17)
+    at startExecution (internal/bootstrap/node.js:276:5)
+
+    ==> An error occurred in the exercise2.js program on line 4 of the program; a ^ points to where JavaScript thinks the error is in the code: it's pointing to the argument list for console.log.
+
+        More specifically, line 6 in the output tells you that a ReferenceError exception occurred and that the name greeting isn't defined. Line 7 repeats some earlier information: JavaScript detected the error at column 15 of line 4 of the program, but it also tells you that the code is in the hello function. Line 8 tells you that hello was called from line 13 of the program in an anonymous function, namely the global-level of the program.
+
+
+  3. Write some code to output the square root of 37.
+
+    ==> console.log(Math.sqrt(37))
+
+
+  4. Given a list of numbers, write some code to find and display the largest numeric value in the list.
+
+    List	              Max
+    1, 6, 3, 2	        6
+    -1, -6, -3, -2	    -1
+    2, 2	              2
+
+    ==> console.log(Math.max(1, 6, 3, 2));      // => 6
+        console.log(Math.max(-1, -6, -3, -2));  // => -1
+        console.log(Math.max(2, 2));            // => 2
+
+
+  5. What does the following function do?
+
+    function doSomething(string) {
+      return string.split(' ').reverse().map((value) => value.length);
+    }
+
+    ==> This code converts a string into an array of words, reverses that array, and then returns a new array that contains the lengths of the words. 
+
+
+  6. Write a function that searches an array of strings for every element that matches the regular expression given by its argument. The function should return all matching elements in an array.
+
+
+    ==> function allMatches(words, pattern) {
+          let matches = [];
+          for (let index = 0; index < words.length; index += 1) {
+            if (pattern.test(words[index])) {
+              matches.push(words[index]);
+            }
+          }
+
+          return matches;
+        }
+
+
+  7. What is exception handling and what problem does it solve?
+
+    ==> Exception handling is a process that deals with errors in a manageable and predictable manner. It uses the try/catch statement to catch exceptions that the code in the try block raises, and lets the programmer deal with the problem in a way that makes sense
+
+
+
+  8. Without using either of those methods, write a function named isNotANumber that returns true if the value passed to it as an argument is NaN, false if it is not.
+
+  ==> function isNotANumber(value) {
+        return value !== value;
+      }
+
+
+  9. Earlier, we learned that JavaScript has multiple versions of the numeric value zero. In particular, it has 0 and -0. While it's mathematically nonsensical to distinguish between 0 and -0, they are distinct values in JavaScript. We won't get into why JavaScript has a 0 and -0, but it can be useful in some cases.
+
+  ==> function isNegativeZero(value) {
+        return 1 / value === -Infinity;
+      }
+
+
+  10. Consider this code:
+
+      > let x = "5"
+      > x = x + 1
+      = "51"
+
+      Now, consider this code:
+
+      > let y = "5"
+      > y++ 
+
+      What gets returned by y++ and why?
+
+      ==> 5 as a numeric value. If you apply ++ to a string, JavaScript coerces it into a number. In this case, "5" gets coerced to the number 5. After coercion, it then increments the value to 6. However, the return value is 5 since the post-increment operator (y++) returns the original value of y, not the incremented value.
+
+      
